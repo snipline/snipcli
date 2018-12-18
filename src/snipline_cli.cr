c007bab31ad72398bf77939907452bc857d3d8bf
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2018-12-18 16:39:51 +0000

    Retrieving Snipline token

--- src/snipline_cli.cr
+++ src/snipline_cli.cr
@@ -1,6 +1,53 @@
 # TODO: Write documentation for `SniplineCli`
+require "admiral"
+require "crest"
+
 module SniplineCli
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+	class Command < Admiral::Command
+		define_version "0.1.0"
+		class Login < Admiral::Command
+			define_help description: "For logging into your Snipline account"
+			def run
+				puts "What's your Snipline email account?"
+				email = gets
+				spawn do 
+					Crest.post(
+						"http://localhost:4001/api/sessions",
+						form: {:email => email}
+					)
+				end
+				puts "Thanks, we're sending you a verification code..."
+				sleep 1
+				puts "Please enter the verification code we sent you:"
+				verification_code = gets
+				puts "Thanks!"
+				puts "Retrieving your long-life token..."
+				Crest.get(
+					"http://localhost:4001/api/tokens/create",
+					params: {:id => email, :token => verification_code}
+				) do |response|
+					puts typeof(response.body)
+					puts response.body.inspect
+				end
+			end
+		end
+
+		class Search < Admiral::Command
+			define_help description: "For searching your snippets"
+			def run
+				puts "Searching..."
+			end
+		end
+
+		register_sub_command :login, Login
+		register_sub_command :search, Search
+
+		# def run
+		# 	puts "help"
+		# end
+	end
 end
+
+SniplineCli::Command.run
