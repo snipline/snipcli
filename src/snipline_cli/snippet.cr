930a0ad41e03db693b1e8fb97eb6338e62e45b5a
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-04-12 15:44:13 +0100

    syntax highlight passwords

--- src/snipline_cli/snippet.cr
+++ src/snipline_cli/snippet.cr
@@ -33,7 +33,10 @@ module SniplineCli
             attributes.tags
         end
 
-        def params() : Array(SnippetParam)
+        #
+        # Params such as variables or select that require input from the user
+        #
+        def interactive_params() : Array(SnippetParam)
             temp_array = [] of SnippetParam
 
             real_command.scan(/#(select)?\{\[(.+?)\]\}/) do |m|
@@ -64,8 +67,26 @@ module SniplineCli
             return temp_array
         end
 
+        #
+        # E.g. Passwords
+        #
+        def uninteractive_params() : Array(SnippetPasswordParam)
+            temp_array = [] of SnippetPasswordParam
+            real_command.scan(/#password\{\[(.+?)\]\}/) do |m|
+                full  = m[1].as(String)
+                options = full.split(",")
+                id = options.shift
+                length = UInt8.new(16)
+                if options.size > 0
+                    length = options.shift.to_u32 || UInt8.new(16)
+                end
+                temp_array << SnippetPasswordParam.new(id, length, full)
+            end
+            temp_array
+        end
+
         def has_params
-            params.size > 0
+            interactive_params.size > 0 || uninteractive_params.size > 0
         end
 
         def preview_command
@@ -75,7 +96,7 @@ module SniplineCli
             end
             
             temp_command = real_command
-            params.each do |param|
+            interactive_params.each do |param|
                 if param.type == "select"
                     temp_command = temp_command.gsub("#select{[#{param.full}]}") { "<#{param.name}>".colorize(:green) }
                     temp_command = temp_command.gsub("#select{[#{param.name}]}") { "<#{param.name}>".colorize(:green) }
@@ -83,6 +104,11 @@ module SniplineCli
                     temp_command = temp_command.gsub("\#{[#{param.full}]}") { "<#{param.name}>".colorize(:green) }
                     temp_command = temp_command.gsub("\#{[#{param.name}]}") { "<#{param.name}>".colorize(:green) }
                 end
+            end
+
+            uninteractive_params.each do |param|
+                temp_command = temp_command.gsub("#password{[#{param.full}]}") { "<PW:#{param.id}>".colorize(:green) }
+                temp_command = temp_command.gsub("#password{[#{param.id}]}") { "<PW:#{param.id}>".colorize(:green) }
             end
             temp_command
         end
@@ -96,6 +122,15 @@ module SniplineCli
         property options : Array(String)
 
         def initialize(@name : String, @default_value : String, @full : String, @type : String, @options : Array(String))
+        end
+    end
+
+    struct SnippetPasswordParam
+        property id : String
+        property length : UInt32 | UInt8
+        property full : String
+
+        def initialize(@id : String, @length : UInt32 | UInt8, @full : String)
         end
     end
 
