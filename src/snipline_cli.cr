0653df7c070b9c2b19c1caa35fd753c15f1fa3cc
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-05-01 12:19:41 +0100

    Add help instructions

--- src/snipline_cli.cr
+++ src/snipline_cli.cr
@@ -11,45 +11,11 @@ module SniplineCli
 
 	class Command < Admiral::Command
 		define_version "0.1.0"
-		class Login < Admiral::Command
-			define_help description: "For logging into your Snipline account"
-			def run
-				puts "What's your Snipline email account?"
-				email = gets
-				spawn do 
-					Crest.post(
-						"http://localhost:4001/api/sessions",
-						form: {:email => email}
-					)
-				end
-				puts "Thanks, we're sending you a verification code..."
-				sleep 1
-				puts "Please enter the verification code we sent you:"
-				verification_code = gets
-				puts "Thanks!"
-				puts "Retrieving your long-life token..."
-				Crest.post(
-					"http://localhost:4001/api/tokens/create",
-					params: {
-						:id => email, 
-						:token => verification_code, 
-						:length => "year"
-					}
-				) do |response|
-					puts typeof(response.body)
-					puts response.body.inspect
-					# config_file = File.open("~/config/.snipline/config.yml")
-					# puts config_file.inspect
-				end
-			end
+		define_help description: "Snipline CLI"
+
+		def run
+			puts "Use #{"snipline login".colorize(:green)} to get started."
 		end
-
-
-		register_sub_command :login, Login
-
-		# def run
-		# 	puts "help"
-		# end
 	end
 end
 
