ff68f6ce49cd0326589ce50bd215a065e6c48f33
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-05-02 11:45:19 +0100

    Add test for tokens

---
+++ spec/models/token_spec.cr
@@ -0,0 +1,7 @@
+describe SniplineCli::Token do
+    it "correctly parses JWT tokens" do
+        content = %({"jwt": "test_token_here"})
+        token = SniplineCli::Token.from_json(content)
+        token.jwt.should eq("test_token_here")
+    end
+end
--- spec/snipline_cli_spec.cr
+++ spec/snipline_cli_spec.cr
@@ -1,9 +1,10 @@
 require "./spec_helper"
+require "./models/*"
 
 describe SniplineCli do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
-  end
+  # it "works" do
+  #   false.should eq(true)
+  # end
 end
