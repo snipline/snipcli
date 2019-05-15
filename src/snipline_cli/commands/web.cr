291318352c46bf49ddee2dbbd51a17d79a0dc602
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-05-15 13:43:25 +0100

    Add assets for web server

--- src/snipline_cli/commands/web.cr
+++ src/snipline_cli/commands/web.cr
@@ -2,6 +2,7 @@ require "toml"
 require "../config/config"
 require "kemal"
 require "file"
+require "../services/file_storage"
 
 module SniplineCli
   class Command < Admiral::Command
@@ -11,6 +12,16 @@ module SniplineCli
       def run
         get "/" do
           render "src/snipline_cli/templates/index.ecr", "src/snipline_cli/templates/layout.ecr"
+        end
+        get "/app.css" do |env|
+          env.response.content_type = "text/css"
+          file = Services::FileStorage.get("css/app.css")
+          file.gets_to_end
+        end
+        get "/app.js" do |env|
+          env.response.content_type = "text/javascript"
+          file = Services::FileStorage.get("js/app.js")
+          file.gets_to_end
         end
         Kemal.run
       end
---
+++ src/snipline_cli/services/file_storage.cr
@@ -0,0 +1,11 @@
+require "baked_file_system"
+
+module SniplineCli
+  module Services
+    class FileStorage
+        extend BakedFileSystem
+
+        bake_folder "../../../public"
+    end
+  end
+end
