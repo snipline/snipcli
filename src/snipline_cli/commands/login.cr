c5c137d52cabb995fb40eaaaffc38d7e693f719b
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-06-04 15:20:55 +0100

    Fix command reference in the login command

--- src/snipline_cli/commands/login.cr
+++ src/snipline_cli/commands/login.cr
@@ -62,7 +62,7 @@ module SniplineCli
             SniplineCli::Services::CreateConfigDirectory.run(SniplineCli.config_file)
             File.write(File.expand_path(SniplineCli.config_file), toml_contents, mode: "w")
             puts "Configuration saved to #{File.expand_path(SniplineCli.config_file).colorize.mode(:bold)}"
-            puts "To fetch your snippets run #{"snipline sync".colorize.mode(:bold)}"
+            puts "To fetch your snippets run #{"snipcli sync".colorize.mode(:bold)}"
           end
         rescue ex : Crest::NotFound
           puts "404 Not Found :("
