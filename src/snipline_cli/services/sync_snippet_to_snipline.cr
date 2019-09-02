3cfb59ad0399e8398f861f76e35d8d762aacaf28
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-09-02 10:18:37 +0100

    Remove TODO comment

--- src/snipline_cli/services/sync_snippet_to_snipline.cr
+++ src/snipline_cli/services/sync_snippet_to_snipline.cr
@@ -5,7 +5,6 @@ module SniplineCli::Services
   class SyncSnippetToSnipline
     # Takes an array of snippets and saves them to the `snippet.json` file.
     def self.handle(snippet_attributes)
-      puts "TODO: Syncing..."
       snippet = Snippet.new(id: nil, type: "snippet", attributes: snippet_attributes)
       SniplineApi.new.create(snippet)
     end
