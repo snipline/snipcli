94df723723f3c180bf2024c868bb8c8bea1be904
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-10-25 13:00:46 +0100

    Syncing local snippets to snipline

--- src/snipline_cli/commands/sync.cr
+++ src/snipline_cli/commands/sync.cr
@@ -19,37 +19,71 @@ module SniplineCli
       def run
         puts "Syncing snippets..."
         config = SniplineCli.config
-				puts config.get("api.token")
 				if config.get("api.token") == ""
 					abort("#{"No API token. Run".colorize(:red)} #{"snipcli login".colorize(:red).mode(:bold)} #{"to login".colorize(:red)}")
 				end
 
 				sync_unsaved_snippets
+				download_snipline_snippets
 
-        # @snipline_api.fetch do |body|
-        #   # Save the response JSON into a file without the data wrapper
-        #   snippets = Models::SnippetDataWrapper.from_json(body).data.to_json
-        #   begin
-        #     @file.store(snippets)
-        #   rescue ex
-        #     puts ex.message
-        #   end
-        # end
       end
 
+
+			def download_snipline_snippets
+        @snipline_api.fetch do |body|
+          # Save the response JSON into a file without the data wrapper
+          cloud_snippets = Models::SnippetDataWrapper.from_json(body).data
+					local_snippets = SniplineCli::Services::LoadSnippets.run
+          begin
+						# Only snippets that are in the cloud but not stored locally
+						difference = [] of SniplineCli::Models::Snippet
+						local_snippet_ids = local_snippets.map { |s| s.id }
+						cloud_snippets.each do |cs|
+							difference << cs unless local_snippet_exists?(cs, local_snippet_ids)
+						end
+						difference.each do |s|
+							puts "Storing #{s.attributes.name} from Snipline".colorize(:green)
+							local_snippets << s
+						end
+						@file.store(local_snippets.to_json) unless difference.size == 0
+          rescue ex
+						puts ex.message.colorize(:red)
+          end
+        end
+			end
+
+			def local_snippet_exists?(cs, local_snippet_ids)
+				unless cs.id.nil?
+					return local_snippet_ids.includes?(cs.id.not_nil!)
+				end
+				return false
+			end
+
 			def sync_unsaved_snippets
-				SniplineCli::Services::LoadSnippets.run.select { |s| s.id.nil? }.each do |snippet|
+				local_snippets = SniplineCli::Services::LoadSnippets.run
+				local_snippets.select { |s| s.id.nil? }.each do |snippet|
 					puts "Attempting to store #{snippet.name.colorize.mode(:bold)} in Snipline..."
 					begin
 						cloud_snippet = SniplineCli::Services::SyncSnippetToSnipline.handle(snippet.attributes)
-						puts "Success!"
+						update_local_snippet_id(cloud_snippet, local_snippets)
+						puts "Success!".colorize(:green)
 					rescue ex : Crest::UnprocessableEntity
 						resp = SniplineCli::Models::SnippetErrorResponse.from_json(ex.response.not_nil!.body)
-						puts "Failed #{resp.errors.first.source["pointer"]}: #{resp.errors.first.detail}".colorize(:red)
+						puts "Failed: #{resp.errors.first.source["pointer"].gsub("/data/attributes/", "")} #{resp.errors.first.detail}".colorize(:red)
 					rescue ex
 						abort ex.message
 					end
 				end
+			end
+
+			def update_local_snippet_id(cloud_snippet, local_snippets)
+				local_snippets = local_snippets.map do |snippet|
+					if snippet.id.nil? && snippet.attributes.snippet_alias == cloud_snippet.attributes.snippet_alias && snippet.name == cloud_snippet.name
+						snippet.id = cloud_snippet.id
+					end
+					snippet
+				end
+				@file.store(local_snippets.to_json)
 			end
     end
 
